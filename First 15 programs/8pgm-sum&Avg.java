fe184da4b1e2d80a0824e7469771859ce41959a0
Fathimathulshafniya <79986209+Fathimathulshafniya@users.noreply.github.com>
2021-05-26 18:16:16 +0530

    Add files via upload

---
+++ First 15 programs/8pgm-sum&Avg.java
@@ -0,0 +1,11 @@
+public class Main
+{
+public static void main(String args[])
+{
+int a=4,b=2,c=9,d=6,e=1;
+int sum=a+b+c+d+e;
+float avg=sum/5;
+System.out.println("The sum of 5 numbers:"+sum);
+System.out.println("The average of 5 numbers:"+avg);
+}
+}
