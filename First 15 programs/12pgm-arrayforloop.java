fd5ad92cf0f86e4bc1bf6d2fce1fe5720a3cc359
Fathimathulshafniya <79986209+Fathimathulshafniya@users.noreply.github.com>
2021-05-26 18:41:57 +0530

    Add files via upload

---
+++ First 15 programs/12pgm-arrayforloop.java
@@ -0,0 +1,12 @@
+public class Main
+{
+public static void main(String args[])
+{
+int a[]={5,3,2,6};
+System.out.println("Array elements are:");
+for(int i=0;i<=a.length;i++)
+ {
+  System.out.println(a[i]);
+ }
+}
+}
---
+++ First 15 programs/13pgm-arrayforeachloop.java
@@ -0,0 +1,12 @@
+public class Main
+{
+public static void main(String args[])
+{
+int a[]={2,4,3,5,1};
+System.out.println("The array elements are:");
+for(int i:a)
+ {
+   System.out.println(i);
+ }
+}
+}
