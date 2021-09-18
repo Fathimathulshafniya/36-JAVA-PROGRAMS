32e02ca7c63aa9a62dbadd57544397c7bdaa879f
Fathimathulshafniya <79986209+Fathimathulshafniya@users.noreply.github.com>
2021-09-18 11:27:00 +0530

    Delete first 15 pgms directory

--- first 15 pgms/main12.java
+++
@@ -1,9 +0,0 @@
-public class main12 {
-    public static void main(String[] args) {
-        int a[]={12,4,5,2};
-        for (int i=0;i<a.length;i++){
-            System.out.println(a[i]);
-        }
-
-    }
-  }
--- first 15 pgms/main13.java
+++
@@ -1,9 +0,0 @@
-public class main13{
-    public static void main(String[] args) {
-        String a[]={"lithin","arun","jibin"};
-        for (String i:a){
-            System.out.println(i);
-
-        }
-    }
-  }
